c3f878af2dbc9b0457d458aeb93afb28d812c705
David Winterbottom <david.winterbottom@gmail.com>
2012-11-29 17:49:30 +0000

    Correct packaging issue with README.md

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '0.1'
+VERSION = '0.1.1'
 
 # Setting for template directory not found by app_directories.Loader.  This
 # allows templates to be identified by two paths which enables a template to be
