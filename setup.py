0ffad0a0fc5dcd27b0888057265ee25952ce0cf4
Michael van Tellingen <michael@mvantellingen.nl>
2015-09-20 21:21:11 +0200

    Update setup files

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 
 from accounts import VERSION
 
+tests_require = [
+    'django-dynamic-fixture==1.8.5',
+    'django-webtest==1.7.8',
+    'pytest==2.8.0',
+    'pytest-cov==2.1.0',
+    'pytest-django==2.8.0',
+]
 
 setup(
     name='django-oscar-accounts',
@@ -21,8 +28,14 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
-        'Programming Language :: Python'],
+        'Programming Language :: Python'
+    ],
     install_requires=[
         'django-oscar>=1.1.1',
         'python-dateutil>=2.4,<2.5'
-    ])
+    ],
+    extras_require={
+        'test': tests_require,
+    },
+    tests_require=tests_require,
+)
