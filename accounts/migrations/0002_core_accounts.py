981b4b78e3fb3e81de59f9f4e0e09f70fb2e8e68
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-18 09:38:25 +0100

    Don't create initial data via django migrations
    Instead supply a management command `oscar_accounts_init`. This also
    allows for faster test runs since we can reuse the test db

--- accounts/migrations/0002_core_accounts.py
+++ accounts/migrations/0002_core_accounts.py
@@ -7,70 +7,8 @@ from accounts import names
 
 
 def create_core_accounts(apps, schema_editor):
-    # Create asset accounts
-
-    # We can't use the treebeard methods here, so these are translated to plain
-    # model / manager methods.
-
-    AccountType = apps.get_model('accounts', 'AccountType')
-
-    # assets = AccountType.add_root(name=names.ASSETS)
-    assets = AccountType.objects.create(
-        path='0001', depth=1, name=names.ASSETS)
-
-    # sales = assets.add_child(name=names.SALES)
-    sales = AccountType.objects.create(
-        path='00010001', depth=2, name=names.SALES)
-    assets.numchild = 1
-    assets.save()
-
-    Account = apps.get_model('accounts', 'Account')
-
-    # sales.accounts.create(name=names.REDEMPTIONS)
-    Account.objects.create(account_type=sales, name=names.REDEMPTIONS)
-
-    # sales.accounts.create(name=names.LAPSED)
-    Account.objects.create(account_type=sales, name=names.LAPSED)
-
-    # cash = assets.add_child(name=names.CASH)
-    cash = AccountType.objects.create(
-        path='00010002', depth=2, name=names.CASH)
-    assets.numchild = 2
-    assets.save()
-
-    # cash.accounts.create(name=names.BANK, credit_limit=None)
-    Account.objects.create(
-        account_type=cash, name=names.BANK, credit_limit=None)
-
-    # unpaid = assets.add_child(name=names.UNPAID_ACCOUNT_TYPE)
-    unpaid = AccountType.objects.create(
-        path='00010003', depth=2, name=names.UNPAID_ACCOUNT_TYPE)
-    assets.numchild = 3
-    assets.save()
-
-    for name in names.UNPAID_ACCOUNTS:
-        # unpaid.accounts.create(name=name, credit_limit=None)
-        Account.objects.create(
-            account_type=unpaid, name=name, credit_limit=None)
-
-    # Create liability accounts
-
-    # liabilities = AccountType.add_root(name=names.LIABILITIES)
-    liabilities = AccountType.objects.create(
-        path='0002', depth=1, name=names.LIABILITIES)
-
-    # income = liabilities.add_child(name=names.DEFERRED_INCOME)
-    income = AccountType.objects.create(
-        path='00020001', depth=2, name=names.DEFERRED_INCOME)
-    liabilities.numchild = 1
-    liabilities.save()
-
-    for i, name in enumerate(names.DEFERRED_INCOME_ACCOUNT_TYPES):
-        # income.add_child(name=name)
-        AccountType.objects.create(
-            path='00020001{:0=4}'.format(i + 1), depth=3, name=name)
-        income.numchild = income.numchild + 1
-        income.save()
+    # Please use `manage.py oscar_accounts_init`
+    pass
 
 
 class Migration(migrations.Migration):
