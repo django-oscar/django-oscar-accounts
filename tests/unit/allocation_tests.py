b8fc125ed79d69fe070f3125d4d7d96ad0d04419
Samir Shah <solaris.smoke@gmail.com>
2017-12-28 08:10:54 +0300

    Fix sandbox views to work with Oscar 1.5
    - Add serialisation and deserialisation helpers to Allocation class, and use these in the sandbox views. We were previously relying on pickle serialisation which is not safe.
    
    - Update Price handling in sandbox views.
    
    - Drop support for Oscar < 1.5 and Django < 1.11
    
    Fixes #55.

--- tests/unit/allocation_tests.py
+++ tests/unit/allocation_tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from oscar_accounts.checkout.allocation import Allocations
 
 
-class TestAllocations(TestCase):
+class AllocationsTestCase(TestCase):
 
     def setUp(self):
         self.allocations = Allocations()
@@ -25,3 +25,15 @@ class TestAllocations(TestCase):
         self.assertEqual(D('10.00'), self.allocations.total)
         self.allocations.remove('A')
         self.assertEqual(D('0.00'), self.allocations.total)
+
+    def test_serialize(self):
+        self.allocations.add('A', D('10.00'))
+        self.allocations.add('B', D('12.00'))
+
+        serialized = Allocations.serialize(self.allocations)
+        self.assertEqual(serialized, '{"A": "10.00", "B": "12.00"}')
+
+    def test_deserialize(self):
+        obj = Allocations.deserialize('{"C": "10.00", "D": "12.00"}')
+        items = list(obj.items())
+        self.assertCountEqual(items, [('C', D('10.00')), ('D', D('12.00'))])
